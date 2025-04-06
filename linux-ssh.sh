10cea95c9308ac494fa43cf98838e5883ad5db00
bkthas <account2@play.run.place>
2025-04-06 10:45:02 +0700

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -23,8 +23,12 @@ fi
 
 echo "### Install ngrok ###"
 
-wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-386.tgz
-sudo tar -xvzf ~/Downloads/ngrok-v3-stable-linux-386.tgz -C /usr/local/bin
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
 
 echo "### Update user: $USER password ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
